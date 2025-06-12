ef0b9d5033db6a1ea0d72829d72c1f2ff74d1b2f
supremelyre <fu.l.r@foxmail.com>
2025-06-12 16:17:51 +0800

    add ros wrapper tasks.json

--- ros_wrapper/src/gici/include/gici/ros_interface/ros_node_handle.h
+++ ros_wrapper/src/gici/include/gici/ros_interface/ros_node_handle.h
@@ -1,53 +1,58 @@
 /**
-* @Function: Handle ROS streams
-*
-* @Author  : Cheng Chi
-* @Email   : chichengcn@sjtu.edu.cn
-*
-* Copyright (C) 2023 by Cheng Chi, All rights reserved.
-**/
+ * @Function: Handle ROS streams
+ *
+ * @Author  : Cheng Chi
+ * @Email   : chichengcn@sjtu.edu.cn
+ *
+ * Copyright (C) 2023 by Cheng Chi, All rights reserved.
+ **/
 #pragma once
 
-#include <iostream>
-#include <vector>
-#include <unordered_map>
 #include <functional>
 #include <glog/logging.h>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 
-#include "gici/stream/node_handle.h"
 #include "gici/ros_interface/ros_stream.h"
+#include "gici/stream/node_handle.h"
 
-namespace gici {
+namespace gici
+{
 
-class RosNodeHandle : public NodeHandle {
-public:
-  RosNodeHandle(ros::NodeHandle& nh, const NodeOptionHandlePtr& nodes);
-  ~RosNodeHandle();
+class RosNodeHandle : public NodeHandle
+{
+  public:
+    RosNodeHandle(ros::NodeHandle &nh, const NodeOptionHandlePtr &nodes);
+    ~RosNodeHandle();
 
-protected:
-  // Bind streamer->formator->ROS-streamer pipelines
-  void bindStreamerToFormatorToRosStreamer(const NodeOptionHandlePtr& nodes);
+  protected:
+    // Bind streamer->formator->ROS-streamer pipelines
+    void bindStreamerToFormatorToRosStreamer(const NodeOptionHandlePtr &nodes);
 
-  // Bind ROS-streamer->formator->streamer pipelines
-  void bindRosStreamerToFormatorToStreamer(const NodeOptionHandlePtr& nodes);
+    // Bind ROS-streamer->formator->streamer pipelines
+    void bindRosStreamerToFormatorToStreamer(const NodeOptionHandlePtr &nodes);
 
-  // Bind estimator->ROS-streamer pipelines
-  void bindEstimatorToRosStreamer(const NodeOptionHandlePtr& nodes);
+    // Bind estimator->ROS-streamer pipelines
+    void bindEstimatorToRosStreamer(const NodeOptionHandlePtr &nodes);
 
-  // Clear streamer->formator->estimator pipeline and rebind them together with
-  // ROS-streamer->estimator pipelines
-  void rebindAllStreamerToEstimator(const NodeOptionHandlePtr& nodes);
+    // Clear streamer->formator->estimator pipeline and rebind them together with
+    // ROS-streamer->estimator pipelines
+    void rebindAllStreamerToEstimator(const NodeOptionHandlePtr &nodes);
 
-  // Get ROS stream from tag
-  inline std::shared_ptr<RosStream> getRosStreamFromTag(std::string tag) {
-    for (auto ros_stream : ros_streams_) {
-      if (ros_stream->getTag() == tag) return ros_stream;
+    // Get ROS stream from tag
+    inline std::shared_ptr<RosStream> getRosStreamFromTag(std::string tag)
+    {
+        for (auto ros_stream : ros_streams_)
+        {
+            if (ros_stream->getTag() == tag)
+                return ros_stream;
+        }
+        return nullptr;
     }
-    return nullptr;
-  }
 
-protected:
-  std::vector<std::shared_ptr<RosStream>> ros_streams_;
+  protected:
+    std::vector<std::shared_ptr<RosStream>> ros_streams_;
 };
 
-}
+} // namespace gici
